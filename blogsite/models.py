a5873651aebf2896832633ab72d0628d95f72a51
chirag <ch84ag.cp@gmail.com>
2019-03-06 14:33:52 +0530

    MinorModelChanges

--- blogsite/models.py
+++ blogsite/models.py
@@ -1,25 +1,30 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.http import HttpResponse
 
-
-# Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete="")
+    author = models.ForeignKey("auth.user", on_delete="")
     tittle = models.CharField(max_length=100)
     text = models.TextField()
-    publication_day = models.DateTimeField()
+    create_date = models.DateTimeField()
+    publication_date = models.DateTimeField(blank=True, null=True)
+
+    # def publish(self):
+    #     if self.author == "user":
+    #         Post.save(self)
+    #     else:
+    #         HttpResponse("Not authorized")
 
     def __str__(self):
-        return self.author
+        return self.tittle
 
 
 class Comments(models.Model):
     post = models.ForeignKey(Post, on_delete="")
     author = models.CharField(max_length=100)
-    text = models.TextField()
+    comment = models.TextField()
     create_date = models.DateTimeField()
-    approve_date = models.DateTimeField()
+    approve = models.BooleanField(default= False)
 
     def __str__(self):
-        return self.post
+        return self.author
